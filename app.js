95140c79249314c3f15f87c0f058f6397ea73036
6202002292 <141506154+6202002292@users.noreply.github.com>
2025-06-25 11:48:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,4 @@
 
-const checkWinner = () => {
-    for(let pattern of winPatterns){
-
-        let pos1Val = boxes[pattern[0]].innerText;
-        let pos2Val = boxes[pattern[1]].innerText;
-        let pos3Val = boxes[pattern[2]].innerText;
-
-        if(pos1Val !== "" && pos2Val !== "" && pos3Val !== ""){
-            if(pos1Val === pos2Val && pos2Val === pos3Val){
-                showWinner(pos1Val);
-            }
-        } 
-    }
-};
-
-newGameBtn.addEventListener("click" , resetGame);
-resetBtn.addEventListener("click" , resetGame);
-
-
-
-
-
-
 const imgs = document.getElementById('imgs')
 const leftBtn = document.getElementById('left')
 const rightBtn = document.getElementById('right')
